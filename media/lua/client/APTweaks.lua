76c6159da0ab564c681d267733747596a7430035
AbrahamPicos <abrahampicos@outlook.com>
2025-05-12 14:07:53 -0700

    Check 5

--- media/lua/client/APTweaks.lua
+++ media/lua/client/APTweaks.lua
@@ -440,7 +440,12 @@ local function OnServerCommand(module, command, args)
             local x, y ,z = args.x, args.y, args.z
 
             player:setHaloNote(format(getText("UI_APTweaks_TeleportSuccess"), player_flags.warpCommandWarp), 0, 255, 0, 500)
-            player:setPosition(x, y, z)
+            player:setLx(x)
+            player:setLy(y)
+            player:setLz(z)
+            player:setX(x)
+            player:setY(y)
+            player:setZ(z)
             -- Debido a que el juego hará un ajuste en la localización del jugador al final de cualquier forma, lo
             --  que llamará de nuevo a RestorePlayerFlags, puede ignorarla aquí.
             -- Aún así las vanderas deben limpiarse ahora para asegurarse de que el comando esté disponble
